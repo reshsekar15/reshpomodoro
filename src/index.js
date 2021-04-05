dc1e05995bcfcfafb1be037094a6e5af35f7e5cc
Reshma <reshsekar15@gmail.com>
2021-04-04 22:31:46 -0500

    more react components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 //just installing react bootstrap isn't enough. Must add the above line to identify the css
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Button, Tabs, Tab, Sonnet, Jumbotron, Nav, Link, NavLink, Collapse, Navbar, NavDropdown } from 'react-bootstrap'
-import { Card, Row, Col} from 'react-bootstrap'
-
+import { Card, Row, Col, Modal, ModalBody, ModalFooter, ModalDialog, Alert, ProgressBar} from 'react-bootstrap'
 
 
 class Square extends React.Component {
@@ -77,7 +76,76 @@ class PamodoroHeader extends React.Component {
     }
 }
 
+class StopWatch extends React.Component {
+    render() {
+        return (
+
+            <>
+                
+            </>
+            )
+        
+    }
+}
+
+//passing in props here from CardSelect component 
+class ShowTimer extends React.Component {
+    render() {
+        console.log("entered")
+        console.log(this.props.basictimer)
+        console.log(this.props.customtimer)
+        if (!this.props.basictimer & !this.props.customtimer) {
+            return (
+                <>
+
+                </>
+            )
+        }
+        else if (this.props.basictimer) {
+            return (
+                <div>
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <Row>
+                        <Col> </Col>
+                        <Card> 
+                            
+                            <Card.Body> 
+                                <Card.Title> Basic Timer </Card.Title>
+                                <p> Displaying stop watch </p>
+                            </Card.Body>
+                        </Card>
+                        <Col> </Col>
+                    </Row>
+                </div>
+            )
+        }
+        else if (this.props.customtimer) {
+            return (
+                <div>
+                    <h1> This is  a custom timer </h1>
+                </div>
+            )
+        }
+        else {
+            <>
+            </>
+        }
+    }
+          
+       
+}
+
 class CardSelect extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            basictimer: false, 
+            customtimer: false
+        };
+    }
     render() {
         return (
             <div>
@@ -93,9 +161,11 @@ class CardSelect extends React.Component {
                             <Card.Body>
                                 <Card.Title>Start Timer</Card.Title>
                                 <Card.Text>
-                                    Already know what activity you would like to work on? Start the timer and get ready
+                                    Already know what activity you would like to work on? 
+                                    Start the timer and get ready to rock!
+
     </Card.Text>
-                                <Button variant="primary">Start Timer</Button>
+                                <Button variant="primary" onClick={() => this.setState({basictimer: true, customtimer: false})}>Start Timer</Button>
                             </Card.Body>
                         </Card>
                     </Col>
@@ -108,13 +178,18 @@ class CardSelect extends React.Component {
                                     Do you like to cherry pick what you want to work on further procastinating activities that don't
                                     interest you? 
     </Card.Text>
-                                <Button variant="primary"> Random activity selector</Button>
+                                <Button variant="primary" onClick={() => this.setState({customtimer: true, basictimer: false})}> Random activity selector</Button>
                             </Card.Body>
                         </Card>
                     </Col>
                     <Col>
                         
                     </Col>
+                </Row>
+                <Row>
+                    <Col> </Col>
+                    <ShowTimer basictimer={this.state.basictimer} customtimer={this.state.customtimer} />
+                    <Col> </Col>
                 </Row>
             </div>);
 
